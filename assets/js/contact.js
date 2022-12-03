aad5726327c08f9e00c16815bab1fc64125e3bbe
amit <adhikariamit2001@gmail.com>
2022-12-03 22:54:16 +0530

    api change

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,9 +3,9 @@ thisForm.addEventListener('submit', async function (e) {
   e.preventDefault();
   const formData = new FormData(thisForm).entries()
   try {
-      const response = await fetch('https://contactme001.herokuapp.com/contact', {
+      const response = await fetch('https://parseapi.back4app.com/classes/contact', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json','X-Parse-Application-Id':'rZLTgwg7f2PtjY31kxwRIpAxMkLzPbsvw9WOnuj6','X-Parse-REST-API-Key':'eL1sgPx7USNw9UeVaW6uzDnHH95hf6BAkEJG3ZeQ' },
       body: JSON.stringify(Object.fromEntries(formData))
     });
     const result = await response.json();
