5c05d2cfde27bc3b80693ffed8566ab9483979f3
amit <adhikariamit2001@gmail.com>
2022-12-03 22:58:44 +0530

    bug fix

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -8,11 +8,9 @@ thisForm.addEventListener('submit', async function (e) {
       headers: { 'Content-Type': 'application/json','X-Parse-Application-Id':'rZLTgwg7f2PtjY31kxwRIpAxMkLzPbsvw9WOnuj6','X-Parse-REST-API-Key':'eL1sgPx7USNw9UeVaW6uzDnHH95hf6BAkEJG3ZeQ' },
       body: JSON.stringify(Object.fromEntries(formData))
     });
-    const result = await response.json();
-    if (result.status == "success") {
+
     document.getElementById("alert").innerHTML = "I get your message. Will contact you as soon as possible."
     thisForm.reset();
-  }
   } catch (error) {
     document.getElementById("alert").innerHTML = "Something Went Wrong !!!. Please Contact via Mobile or other social Handels."
   } 
