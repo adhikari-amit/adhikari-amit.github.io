fc7f9f31e1174580e686f884ae22f6ee3341a733
amit <adhikariamit2001@gmail.com>
2023-01-04 00:13:19 +0530

    Captcha Added

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,17 +1,13 @@
-const thisForm = document.getElementById('contactForm');
-thisForm.addEventListener('submit', async function (e) {
-  e.preventDefault();
-  const formData = new FormData(thisForm).entries()
-  try {
-      const response = await fetch('https://parseapi.back4app.com/classes/contact', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json','X-Parse-Application-Id':'rZLTgwg7f2PtjY31kxwRIpAxMkLzPbsvw9WOnuj6','X-Parse-REST-API-Key':'eL1sgPx7USNw9UeVaW6uzDnHH95hf6BAkEJG3ZeQ' },
-      body: JSON.stringify(Object.fromEntries(formData))
-    });
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+      event.preventDefault();
+      emailjs.sendForm('service_fc9953k', 'template_l671poe', '#contact-form')
+          .then(function (response) {
+              console.log('SUCCESS!', response.status, response.text);
+              document.getElementById("success").innerHTML = "I get your message. Will contact you as soon as possible."
+              document.getElementById('contact-form').reset();
+          }, function (error) {
+             document.getElementById("alert").innerHTML = "Something Went Wrong !!!. Please Contact via Mobile or other social Handels."
+              console.log('FAILED...', error);
+          });
 
-    document.getElementById("alert").innerHTML = "I get your message. Will contact you as soon as possible."
-    thisForm.reset();
-  } catch (error) {
-    document.getElementById("alert").innerHTML = "Something Went Wrong !!!. Please Contact via Mobile or other social Handels."
-  } 
-});
+  });
